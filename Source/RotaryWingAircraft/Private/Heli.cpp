9a2480e22f95648f12075cc81373f229f4e3b4e5
Danny McGee <dannymcgee@gmail.com>
2023-02-18 20:40:51 -0500

    Migrate pawn to plugin

---
+++ Source/RotaryWingAircraft/Private/Heli.cpp
@@ -0,0 +1,99 @@
+﻿#include "Heli.h"
+#include "HeliMvmtCmp.h"
+
+
+// Initialization --------------------------------------------------------------
+
+AHeli::AHeli() : Super() {
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+
+	_Mesh = InitSkelMesh();
+	RootComponent = _Mesh;
+
+	_VehicleMovement = InitVehicleMovement(_Mesh);
+}
+
+auto AHeli::InitSkelMesh() -> USkeletalMeshComponent* {
+	auto* mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VehicleMesh"));
+	mesh->SetCollisionProfileName(UCollisionProfile::Vehicle_ProfileName);
+	mesh->BodyInstance.bSimulatePhysics = true;
+	mesh->BodyInstance.bNotifyRigidBodyCollision = true;
+	mesh->BodyInstance.bUseCCD = true;
+	mesh->bBlendPhysics = true;
+	mesh->SetGenerateOverlapEvents(true);
+	mesh->SetCanEverAffectNavigation(false); // TODO: Is this correct?
+
+	return mesh;
+}
+
+auto AHeli::InitVehicleMovement(USkeletalMeshComponent* mesh) -> UHeliMvmtCmp* {
+	auto* cmp = CreateDefaultSubobject<UHeliMvmtCmp>(TEXT("VehicleMovement"));
+	cmp->SetIsReplicated(true);
+	cmp->UpdatedComponent = mesh;
+
+	return cmp;
+}
+
+// TODO: Look at Lyra to come up with a better way to setup these inputs
+void AHeli::SetupPlayerInputComponent(UInputComponent* input) {
+	input->BindAction("StartEngine", IE_Pressed, _VehicleMovement, &UHeliMvmtCmp::StartEngine);
+	input->BindAction("StopEngine", IE_Pressed, _VehicleMovement, &UHeliMvmtCmp::StopEngine);
+
+	input->BindAxis("CollectiveUp", this, &AHeli::OnCollectiveUp);
+	input->BindAxis("CollectiveDown", this, &AHeli::OnCollectiveDown);
+
+	input->BindAction("YawRight", IE_Pressed, this, &AHeli::IncYaw);
+	input->BindAction("YawRight", IE_Released, this, &AHeli::DecYaw);
+	input->BindAction("YawLeft", IE_Pressed, this, &AHeli::DecYaw);
+	input->BindAction("YawLeft", IE_Released, this, &AHeli::IncYaw);
+
+	input->BindAxis("Pitch", _VehicleMovement, &UHeliMvmtCmp::SetPitchInput);
+	input->BindAxis("Roll", _VehicleMovement, &UHeliMvmtCmp::SetRollInput);
+}
+
+
+// Update ----------------------------------------------------------------------
+
+void AHeli::Tick(float deltaTime) {
+	Super::Tick(deltaTime);
+
+	_VehicleMovement->SetYawInput(_YawInput);
+	_VehicleMovement->SetCollectiveInput(_CollectiveInput);
+
+	_CollectiveInput = 0;
+}
+
+
+// Getters ---------------------------------------------------------------------
+
+auto AHeli::GetMesh() const -> USkeletalMeshComponent* {
+	return _Mesh;
+}
+
+auto AHeli::GetVehicleMovement() const -> UHeliMvmtCmp* {
+	return _VehicleMovement;
+}
+
+auto AHeli::GetMovementComponent() const -> UPawnMovementComponent* {
+	return _VehicleMovement;
+}
+
+
+// Input handling --------------------------------------------------------------
+
+void AHeli::OnCollectiveUp(float input) {
+	_CollectiveInput = FMath::Clamp(_CollectiveInput + input, -1, 1);
+}
+
+void AHeli::OnCollectiveDown(float input) {
+	_CollectiveInput = FMath::Clamp(_CollectiveInput - input, -1, 1);
+}
+
+void AHeli::IncYaw() {
+	_YawInput += 1;
+}
+
+void AHeli::DecYaw() {
+	_YawInput -= 1;
+}
---
+++ Source/RotaryWingAircraft/Public/Heli.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Heli.generated.h"
+
+class UHeliMvmtCmp;
+
+
+UCLASS(Blueprintable)
+class ROTARYWINGAIRCRAFT_API AHeli : public APawn {
+	GENERATED_BODY()
+	
+
+public:
+
+	AHeli();
+
+	virtual void Tick(float deltaTime) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* input) override;
+
+	UFUNCTION(BlueprintGetter)
+	USkeletalMeshComponent* GetMesh() const;
+
+	UFUNCTION(BlueprintGetter)
+	UHeliMvmtCmp* GetVehicleMovement() const;
+
+	virtual auto GetMovementComponent() const -> UPawnMovementComponent* override;
+
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter="GetMesh", Category="Vehicle")
+	USkeletalMeshComponent* _Mesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter="GetVehicleMovement", Category="Vehicle")
+	UHeliMvmtCmp* _VehicleMovement;
+
+	float _CollectiveInput = 0;
+	float _YawInput = 0;
+
+	auto InitSkelMesh() -> USkeletalMeshComponent*;
+	auto InitVehicleMovement(USkeletalMeshComponent* mesh) -> UHeliMvmtCmp*;
+
+	void OnCollectiveUp(float input);
+	void OnCollectiveDown(float input);
+
+	void IncYaw();
+	void DecYaw();
+};
